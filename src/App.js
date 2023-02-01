dfb0a96bfabbc9b119396100660c035533addbdd
SMARTSOFT\L.Dudauri <l.dudauri@smartsoft.ge>
2023-02-01 17:48:37 +0400

    dd

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import { BrowserRouter , Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <div className='test' > test </div>
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Rate />} />
