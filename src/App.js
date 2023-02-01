c29a90b08cbf1db2ffccc767152f350df41813ef
SMARTSOFT\L.Dudauri <l.dudauri@smartsoft.ge>
2023-02-01 18:11:47 +0400

    ddds

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import './App.css';
 import React from 'react';
 import Rate from './Pages/Rate/Rate';
 import Selected from "./Pages/Selected/Selected";
-import { BrowserRouter , Route, Routes } from "react-router-dom";
+import { BrowserRouter , Route, Routes , HashRouter} from "react-router-dom";
 function App() {
   return (
     <div className="App">
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<Rate />} />
                 <Route path="/Selected" element={<Selected />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     </div>
   );
 }
